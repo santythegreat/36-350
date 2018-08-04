adc6306fc3c99e6e1ac0c6279fc7e8950de81724
Santiago Roa <santiagoroa@Santiagos-Air.wv.cc.cmu.edu>
2018-08-03 21:22:09 -0400

    added model_select function that returns values from linear model with p-values <= cutoff

--- lab_12_simulation.R
+++ lab_12_simulation.R
@@ -3,3 +3,9 @@ generate_data = function(n, p) {
   responses = rep(rnorm(1),3^2)
   covariates = matrix(responses,n,n)
 }
+
+### 2b
+model_select = function(covariates, responses, cutoff) {
+  linearModel = lm(responses ~ covariates)
+  linearModel = linearModel[which((summary(linearModel)$coefficients[c("responses","covariates"),4] <= cutoff))]
+}
