21834cb86ab70ac0aa3973971e393923bafa74c5
Santiago Roa <santiagoroa@Santiagos-Air.wv.cc.cmu.edu>
2018-08-03 21:28:40 -0400

    forgot to add the second part of the problem which was returning the p-values of another linear model

--- lab_12_simulation.R
+++ lab_12_simulation.R
@@ -8,4 +8,8 @@ generate_data = function(n, p) {
 model_select = function(covariates, responses, cutoff) {
   linearModel = lm(responses ~ covariates)
   linearModel = linearModel[which((summary(linearModel)$coefficients[c("responses","covariates"),4] <= cutoff))]
+  if(length(linearModel) == 0) return(vector())
+  newLinearModel = lm(linearModel ~ covariates)
+  return(summary(newLinearModel)$coefficients[c("linearModel","covariates"),4])
 }
+
